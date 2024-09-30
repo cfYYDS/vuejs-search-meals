553ab26cf16701524bf6a26a49312dcc3a4b25b6
CF <89397175+cfYYDS@users.noreply.github.com>
2024-09-30 17:50:11 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base:'vuejs-search-meals-3sz9g.kinsta.page/',
+ base:'vuejs-search-meals-3sz9g.kinsta.page',
   plugins: [vue()],
 })
